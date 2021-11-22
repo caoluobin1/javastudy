465eb03451387eca6a5500d72e2d2f9bd9ba2845
cao_luobin <l>
2021-11-22 17:53:26 +0800

    1

--- src/main/java/org/clb/structure/binary_tree/BTreeUtil.java
+++ src/main/java/org/clb/structure/binary_tree/BTreeUtil.java
@@ -2,6 +2,10 @@ package org.clb.structure.binary_tree;
 
 import org.clb.pojo.list.BTreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 /**
  * @Description
  * @Classname BTreeUtil
@@ -33,6 +37,33 @@ public class BTreeUtil {
         a.setLeft(b);a.setRight(c);
         b.setLeft(d);
         c.setLeft(f);c.setRight(g);
+        return a;
+    }
+
+    public static BTreeNode generateBTree() {
+        BTreeNode a = new BTreeNode("1");
+        int count = (int) Math.random() * 2 + 1;
+        Queue<BTreeNode> queue = new LinkedList<>();
+        queue.add(a);
+        for (int i = 0; i < count; i++) {
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                BTreeNode now = queue.poll();
+                for (int k = 0; k < 2; k++) {
+                    int flag = (int) Math.random() * 2 + 1;
+                    if (k == 0 && flag == 1) {
+                        BTreeNode left = new BTreeNode((int) Math.random() * 10000 + "");
+                        now.left = left;
+                        queue.add(left);
+                    }
+                    if (k == 1 && flag == 1) {
+                        BTreeNode right = new BTreeNode((int) Math.random() * 10000 + "");
+                        now.right = right;
+                        queue.add(right);
+                    }
+                }
+            }
+        }
         return a;
     }
 }
---
+++ src/main/java/org/clb/structure/binary_tree/BinaryTreeCode13_3.java
@@ -0,0 +1,62 @@
+package org.clb.structure.binary_tree;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.clb.pojo.list.BTreeNode;
+
+/**
+ * @Description 是否是搜索二叉树
+ * @Classname BinaryTreeCode13_1
+ * @Date 2021/11/21 13:34
+ * @Author clb
+ */
+public class BinaryTreeCode13_3 {
+    public static boolean isBST(BTreeNode head) {
+        return process(head).isBST;
+    }
+
+    public static Info process(BTreeNode x) {
+        if (x == null) {
+            return null;
+        }
+        Info left = process(x.left);
+        Info right = process(x.right);
+        boolean isBST = true;
+        int max = 0;
+        int min = 0;
+        if (left != null && !left.isBST) {
+            isBST = left.isBST;
+            max = Math.max(left.max, Integer.parseInt(x.value));
+            min = Math.max(left.min, Integer.parseInt(x.value));
+        }
+        if (right != null && !right.isBST) {
+            isBST = right.isBST;
+            max = Math.max(right.max, Integer.parseInt(x.value));
+            min = Math.max(right.min, Integer.parseInt(x.value));
+        }
+        if (left != null && left.max >= Integer.parseInt(x.value)) {
+            isBST = false;
+        }
+        if (right != null && right.max <= Integer.parseInt(x.value)) {
+            isBST = false;
+        }
+        return new Info(isBST, max, min);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBST(BTreeUtil.generateBTree()));
+    }
+
+    /**
+     * @param
+     * @description: 条件1.左子树的最大值小于当前节点 2.右子树的最小值大于当前节点
+     * @return:
+     */
+    @Data
+    @AllArgsConstructor
+    public static class Info {
+        public boolean isBST;
+        public int max;
+        public int min;
+    }
+}
