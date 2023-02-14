7df4065322b2a92ce163adcc6e30b840f0862da3
caoluobin <clb@ngemail.rd.ngsoft.com>
2023-02-14 18:03:47 +0800

    ThreadLocalRandom

--- src/main/java/org/clb/juc/ThreadLocalRandomTest.java
+++ src/main/java/org/clb/juc/ThreadLocalRandomTest.java
@@ -1,5 +1,8 @@
 package org.clb.juc;
 
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
@@ -14,14 +17,17 @@ public class ThreadLocalRandomTest {
             TimeUnit.SECONDS.sleep(1);
             new Thread(() ->{
                 System.out.println(current.nextInt());
-                System.out.println(current.nextInt());
+//                System.out.println(current.nextInt());
             }).start();
         }
         TimeUnit.SECONDS.sleep(1);
         System.out.println("==========================================");
         for (int i = 0; i < 10; i++) {
             new Thread(() ->{
-                System.out.println(ThreadLocalRandom.current().nextInt());
+                ThreadLocalRandom localRandom = ThreadLocalRandom.current();
+                MetaObject ra = SystemMetaObject.forObject(localRandom);
+                System.out.println(ra.getValue("seed"));
+                System.out.println(localRandom.nextInt());
             }).start();
         }
     }
